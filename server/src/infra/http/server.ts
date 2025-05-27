c5507269e13de02eca3b4b2980fb90f85a625863
hugowemer <wemer.eng@gmail.com>
2025-05-27 18:28:11 -0300

    feat: add initial Fastify server and sample POST route for links

--- server/src/infra/http/server.ts
+++ server/src/infra/http/server.ts
@@ -1,12 +1,32 @@
 import { env } from '@/env'
 import { fastifyCors } from '@fastify/cors'
 import { fastify } from 'fastify'
+import {
+  hasZodFastifySchemaValidationErrors,
+  serializerCompiler,
+  validatorCompiler,
+} from 'fastify-type-provider-zod'
+import { createLinkRoute } from './routes/create-link'
 
 const server = fastify()
 
+server.setValidatorCompiler(validatorCompiler)
+server.setSerializerCompiler(serializerCompiler)
+
+server.setErrorHandler((error, request, reply) => {
+  if (hasZodFastifySchemaValidationErrors(error)) {
+    return reply.status(400).send({
+      message: 'Validation error',
+      issues: error.validation,
+    })
+  }
+  console.error(error)
+  return reply.send(500).send({ message: 'Internal server error' })
+})
+
 server.register(fastifyCors, { origin: '*' })
 
-console.log(env.DATABASE_URL)
+server.register(createLinkRoute)
 
 server.listen({ port: 3333, host: '0.0.0.0' }).then(() => {
   console.log('HTTP Server running!')
