ee6efda220247ff5f34c552bc06b4ebcb033d5d6
hugowemer <wemer.eng@gmail.com>
2025-05-27 21:21:46 -0300

    chore: configure Swagger and scaffold test structure

--- server/src/env.ts
+++ server/src/env.ts
@@ -3,6 +3,7 @@ import { z } from 'zod'
 const envSchema = z.object({
   PORT: z.coerce.number().default(3333),
   DATABASE_URL: z.string().url().startsWith('postgresql://'),
+  NODE_ENV: z.enum(['development', 'test', 'production']).default('production'),
 })
 
 export const env = envSchema.parse(process.env)
--- server/src/infra/http/routes/create-link.ts
+++ server/src/infra/http/routes/create-link.ts
@@ -1,7 +1,30 @@
 import type { FastifyPluginAsyncZod } from 'fastify-type-provider-zod'
+import { z } from 'zod'
 
 export const createLinkRoute: FastifyPluginAsyncZod = async server => {
-  server.post('/link', () => {
-    return 'Hello Link'
-  })
+  server.post(
+    '/link',
+    {
+      schema: {
+        summary: 'Create new link',
+        body: z.object({
+          link: z.string().url(),
+          shortenLink: z.string().url(),
+        }),
+        response: {
+          201: z.object({
+            linkId: z.string(),
+          }),
+          409: z
+            .object({
+              message: z.string(),
+            })
+            .describe('Link already exists.'),
+        },
+      },
+    },
+    async (request, reply) => {
+      return reply.status(201).send({ linkId: 'teste' })
+    }
+  )
 }
